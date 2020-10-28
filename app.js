38a2f8635baea121ab335a72c892cfb58bdfa601
greg-el <gr3gl@hotmail.com>
2020-10-28 17:25:10 +0000

    update server port

--- app.js
+++ app.js
@@ -101,7 +101,6 @@ app.post("/getNearestSellers/", (req, res) => {
 
 var server_port = process.env.PORT || 3000;
 var server_host = "0.0.0.0";
-server.listen(server_port, server_host, function () {
+app.listen(server_port, server_host, function () {
   console.log("Listening on port %d", server_port);
 });
-app.listen(process.env.PORT || 3000);
